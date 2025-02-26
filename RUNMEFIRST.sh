d2d2d295e82b043f43c763843e1ae97f299e9d8d
Omkar Rajesh Rathod <omkarrathod0705@gmail.com>
2025-02-26 18:46:23 +0530

    RUNMEFIRST.sh

--- RUNMEFIRST.sh
+++ RUNMEFIRST.sh
@@ -3,4 +3,5 @@
 #!/bin/bash
 
 cd /etc
-echo "jenkins ALL=(ALL) NOPASSWD:ALL" >> sudoers
+echo "jenkins ALL=(ALL) NOPASSWD:ALL" >> sudoers  # To tell system to no passward needed 
+usermod -a -G root jenkins                        # adding jenkins user to root group
